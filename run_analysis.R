bec96f949717de8e5e717ef94a6275c5a16b7622
sophia <sofernan@cisco.com>
2014-07-26 17:54:00 -0700

    latest version of file

--- run_analysis.R
+++ run_analysis.R
@@ -29,8 +29,8 @@ library(reshape2)
 test_melt <- melt(test_X_sub,id = c("V1","test_label_activity$V2"))
 names(test_melt)[1]<-"subject"
 names(test_melt)[2]<-"activity"
-names(test_melt)[3]<-"testname"
-names(test_melt)[4]<-"testvalue"
+names(test_melt)[3]<-"featurename"
+names(test_melt)[4]<-"featurevalue"
 # End Creation Narrow Data Set for Test Set
 
 # Follow same process for train data
@@ -50,8 +50,8 @@ train_melt <- NULL
 train_melt <- melt(train_X_sub,id = c("V1","train_label_activity$V2"))
 names(train_melt)[1]<-"subject"
 names(train_melt)[2]<-"activity"
-names(train_melt)[3]<-"testname"
-names(train_melt)[4]<-"testvalue"
+names(train_melt)[3]<-"featurename"
+names(train_melt)[4]<-"featurevalue"
 # End Creation Narrow Data Set for train Set
 
 # Merge test and train sets
@@ -61,7 +61,7 @@ all_melt <- rbind (test_melt, train_melt)
 #Creates a second, independent tidy data set with the average of 
 #each variable for each activity and each subject. 
 tidy_data <- NULL
-tidy_data <- ddply(all_melt, c("subject","activity", "testname"), summarise,
-               mean = mean(testvalue));
+tidy_data <- ddply(all_melt, c("subject","activity", "featurename"), summarise,
+               mean = mean(featurevalue));
 write.csv(tidy_data,"tidy_data_fin.csv")
 dim(tidy_data)
